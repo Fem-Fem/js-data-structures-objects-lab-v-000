1281c9ae74c16e74e169216e3539245bbbe1a1aa
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-28 22:32:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const driver = {};
+
+// function updateDriverWithKeyAndValue(driver, key, value) {
+//   const newObj = { ...driver};
+//   newObj[key] = value;
+//   return newObj;
+// }
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  // newObj = { ...driver };
+  newObj[key] = value;
+  return newObj;
+
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  // delete driver.key;
+  newObj = Object.assign({}, driver)
+  delete driver[key];
+  return driver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
